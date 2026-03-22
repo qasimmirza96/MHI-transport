e98305131158724fda75fb3f98bc25dcfa370030
IamArslanZafar <Imarslanzafar@gmail.com>
2026-03-22 19:43:00 +0500

    Simplify Layout component signature

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Header from "../Components/Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../Components/Footer/Footer";
 
-const Layout = ({ Children }) => {
+const Layout = () => {
   const [isNav, setIsNav] = useState(false);
   return (
     <div className="layoutWrapper">
